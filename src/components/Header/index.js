2d3a899a786c4f363bd112115ebd27a3e865fe10
swapnil794 <swapnilvivo@gmail.com>
2021-07-19 03:36:31 +0530

    smovies

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.Styles';
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+const Header = () => (
+  <Wrapper>
+    <Content>
+      <LogoImg src={RMDBLogo} alt='rmdb-logo' />
+      <TMDBLogoImg src={TMDBLogo} alt='tmdb_logo' />
+    </Content>
+  </Wrapper>
+);
+
+export default Header;
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Wrapper, Content } from './SearchBar';
+import SearchIcon from '../../images/search-icon.svg';
+
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState('');
+  const initial = useRef(true);
+  useEffect(() => {
+    if (initial.current) {
+      initial.current = false;
+      return;
+    }
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [setSearchTerm, state]);
+  return (
+    <Wrapper>
+      <Content>
+        <img src={SearchIcon} alt='search-icon' />
+        <input
+          type='text'
+          placeholder='Search Movies'
+          onChange={(event) => setState(event.currentTarget.value)}
+          value={state}
+        />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default SearchBar;
